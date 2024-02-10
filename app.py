ec6e08ac9c3f496fae74028f8cdbe76425601efb
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-02-10 22:19:50 +0100

    feat: all required data queried

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 import asyncio
 
 load_dotenv()
-ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 current_year = datetime.now().year
 
@@ -37,6 +36,7 @@ class FinancialDataTypeSwitch:
         self.year_range = range(4)
         self.errorState = []
         self.current_company = None
+        self.api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
 
     def add_error_to_error_state(self, error):
         self.errorState.append(error)
@@ -53,8 +53,7 @@ class FinancialDataTypeSwitch:
         return self.financial_data_aggregate
 
     async def get_data(self, function_type, symbol):
-        api_key = ALPHA_VANTAGE_API_KEY
-        api_url = f'https://www.alphavantage.co/query?function={function_type}&symbol={symbol}&apikey={api_key}'
+        api_url = f'https://www.alphavantage.co/query?function={function_type}&symbol={symbol}&apikey={self.api_key}'
         response = requests.get(api_url)
         self.set_current_company(symbol)
         if response.status_code == 200:
@@ -62,6 +61,38 @@ class FinancialDataTypeSwitch:
             self.process_data(function_type, data)
         else:
             error = jsonify({'error': f'Failed to fetch {function_type} data for {symbol}'})
+            self.add_error_to_error_state(error)
+
+    async def get_overview_data(self, symbol):
+        api_url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={self.api_key}'
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            self.add_to_financial_data_aggregate('BETA', data['Beta'])
+        else:
+            error = jsonify({'error': f'Failed to fetch overview data for {symbol}'})
+            self.add_error_to_error_state(error)
+
+    async def get_price_data(self, symbol):
+        api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={self.api_key}'
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            latest_daily_price_date, latest_daily_price = next(iter(data["Time Series (Daily)"].items()))
+            self.add_to_financial_data_aggregate('LATEST_PRICE', latest_daily_price["4. close"])
+            self.add_to_financial_data_aggregate('LATEST_PRICE_DATE', latest_daily_price_date)
+        else:
+            error = jsonify({'error': f'Failed to fetch overview data for {symbol}'})
+            self.add_error_to_error_state(error)
+
+    async def get_treasury_data(self, symbol):
+        api_url = f'https://www.alphavantage.co/query?function=TREASURY_YIELD&symbol={symbol}&apikey={self.api_key}'
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            self.add_to_financial_data_aggregate('TREASURY_YIELD_10_YEARS', )
+        else:
+            error = jsonify({'error': f'Failed to fetch treasury data for {symbol}'})
             self.add_error_to_error_state(error)
 
     def process_data(self, function_type, data):
@@ -77,9 +108,15 @@ class FinancialDataTypeSwitch:
                                              get_sub_category_data(data=data, year_range=self.year_range, keys=keys))
 
     def income_statement(self, data):
-        keys = ['totalRevenue', 'netIncome']
+        keys = ['totalRevenue', 'netIncome', 'incomeBeforeTax', 'interestAndDebtExpense', 'incomeTaxExpense']
         get_sub_category_data(data=data, year_range=self.year_range, keys=keys)
         self.add_to_financial_data_aggregate('INCOME_STATEMENT',
+                                             get_sub_category_data(data=data, year_range=self.year_range, keys=keys))
+
+    def balance_sheet(self, data):
+        keys = ['commonStockSharesOutstanding']
+        get_sub_category_data(data=data, year_range=self.year_range, keys=keys)
+        self.add_to_financial_data_aggregate('BALANCE_SHEET',
                                              get_sub_category_data(data=data, year_range=self.year_range, keys=keys))
 
 
@@ -89,11 +126,13 @@ financial_data_aggregator = FinancialDataTypeSwitch()
 @app.route('/')
 async def main_page_finance_data():
     symbols = ['AAPL', 'META', 'IBM']
-    function_types = ['CASH_FLOW', 'INCOME_STATEMENT']
+    function_types = ['CASH_FLOW', 'INCOME_STATEMENT', 'BALANCE_SHEET']
     tasks = []
     for function_type in function_types:
         for symbol in symbols:
             tasks.append(financial_data_aggregator.get_data(function_type, symbol))
+            tasks.append(financial_data_aggregator.get_overview_data(symbol))
+            tasks.append(financial_data_aggregator.get_price_data(symbol))
     await asyncio.gather(*tasks)
     return render_template('data.html', data=financial_data_aggregator.financial_data_aggregate)
 
