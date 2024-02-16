97f3cb0ac422a0f6d3ba97a149d35a95481990f0
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-02-16 22:16:42 +0100

    feat: completed DCF calculation

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ current_year = datetime.now().year
 app = Flask(__name__)
 
 # Percentage of market return rate estimate
-MARKET_RETURN_RATE = 10
+MARKET_RETURN_RATE = 0.1
 
 # Perpetual growth estimate
 
@@ -26,22 +26,32 @@ def calculate_percentage_difference(array):
         next_element = array[i + 1]
         percentage_difference = ((next_element - current_element) / current_element)
         percentage_differences.append(percentage_difference)
-        average = sum(percentage_differences) / len(percentage_differences)
-        if average <= 0:
-            return max(percentage_differences)
+    print('percentage_differences:', percentage_differences)
+    average = sum(percentage_differences) / len(percentage_differences)
+    print('average:', average)
+    if average <= 0:
+        maxDif = max(percentage_differences)
+        if maxDif < 0:
+            return 1
         else:
-            return average
+            return max(percentage_differences)
+    else:
+        return average
 
 
 def calculate_growth(initial_value, growth_rate, periods):
     return [initial_value * (1 + growth_rate) ** n for n in range(1, periods + 1)]
 
-def generate_interest_rates(initial_rate, n, num_rates):
-    rates = [initial_rate]  # Initialize the list with the initial rate
-    for _ in range(1, num_rates):
-        next_rate = rates[-1] * n  # Calculate the next rate by multiplying the previous rate by n
-        rates.append(next_rate)  # Add the next rate to the list
+
+def generate_interest_rates(return_multiplier, total_periods):
+    rates = [return_multiplier]
+    for _ in range(1, total_periods):
+        next_rate = rates[-1] * return_multiplier
+        rates.append(next_rate)
+    rates.append(rates[-1])
+    print('rates',rates)
     return rates
+
 
 class CalculateSignal:
     def __init__(self):
@@ -54,14 +64,15 @@ class CalculateSignal:
         total_net_income_periods = list(map(int, data[symbol]['INCOME_STATEMENT']['netIncome']))
         total_revenue_periods = list(map(int, data[symbol]['INCOME_STATEMENT']['totalRevenue']))
         period_number = len(total_net_income_periods)
+        total_debt = int(data[symbol]['BALANCE_SHEET']['shortTermDebt'][0]) + int(data[symbol]['BALANCE_SHEET']['longTermDebt'][0])
 
         self.calc_fcfe_net_income_ratio(symbol, data, total_net_income_periods)
-        self.calc_net_income_margin(symbol, data, total_net_income_periods)
-        self.calc_earnings_growth(symbol, total_net_income_periods)
+        self.calc_net_income_margin(symbol, total_net_income_periods, total_revenue_periods)
+        self.calc_earnings_growth(symbol, total_revenue_periods)
         self.calc_projected_free_cash_flow(symbol, total_revenue_periods, 4)
-        self.calc_wacc(data, symbol, global_data)
+        self.calc_wacc(data, symbol, global_data, total_debt)
         self.calc_terminal_value(symbol)
-        self.calc_dfc(symbol, period_number)
+        self.calc_dfc(symbol, period_number,data)
 
     # Calculate the average Free Cash Flow to Equity / Net Income ratio for the time period
     def calc_fcfe_net_income_ratio(self, symbol, data, total_net_income_periods):
@@ -75,23 +86,28 @@ class CalculateSignal:
         self.signals[symbol]['FCFE_NET_INCOME_RATIO'] = sum(fcfe_net_income_ratio) / len(fcfe_net_income_ratio)
 
     # Calculate the net income margin and take the minimum for the most conservative estimate
-    def calc_net_income_margin(self, symbol, data, total_net_income_periods):
-        total_revenue_periods = list(map(int, data[symbol]['INCOME_STATEMENT']['totalRevenue']))
+    def calc_net_income_margin(self, symbol, total_net_income_periods, total_revenue_periods):
 
         net_income_margin = [total_net_income_period / total_revenue_period for
                              total_net_income_period, total_revenue_period in
                              zip(total_net_income_periods, total_revenue_periods)]
-        self.signals[symbol]['NET_INCOME_MARGIN'] = min(net_income_margin) + 1
+        print('net_income_margin:', net_income_margin)
+        self.signals[symbol]['NET_INCOME_MARGIN'] = min(net_income_margin)
 
-    def calc_earnings_growth(self, symbol, total_net_income_periods):
-        self.signals[symbol]['EARNINGS_GROWTH_RATE'] = calculate_percentage_difference(total_net_income_periods)
+    def calc_earnings_growth(self, symbol, total_revenue_periods):
+        self.signals[symbol]['EARNINGS_GROWTH_RATE'] = calculate_percentage_difference(total_revenue_periods)
 
     def calc_projected_free_cash_flow(self, symbol, total_revenue_periods, period):
+        print('growth_rate', self.signals[symbol]['EARNINGS_GROWTH_RATE'])
+        print('revenue_basic', total_revenue_periods[0])
         projected_revenue = calculate_growth(total_revenue_periods[0], self.signals[symbol]['EARNINGS_GROWTH_RATE'],
                                              period)
+        print('projected_revenue', projected_revenue)
         projected_net_income = [self.signals[symbol]['NET_INCOME_MARGIN'] * revenue for revenue in projected_revenue]
+        print('projected_net_income', projected_net_income)
         projected_free_cash_flows = [self.signals[symbol]['FCFE_NET_INCOME_RATIO'] * net_income for net_income in
                                      projected_net_income]
+        print('projected_fcf', projected_free_cash_flows)
         self.signals[symbol]['PROJECTED_FREE_CASH_FLOWS'] = projected_free_cash_flows
 
     def calc_effective_tax_rate(self, data, symbol):
@@ -102,47 +118,66 @@ class CalculateSignal:
         else:
             return abs(income_tax_expense_latest / income_before_tax_latest)
 
-    def calc_debt_cost_wacc(self, data, symbol):
+    def calc_debt_cost_wacc(self, data, symbol,total_debt):
         interest_expense = data[symbol]['INCOME_STATEMENT']['interestExpense'][0]
-        total_debt = data[symbol]['BALANCE_SHEET']['shortLongTermDebtTotal'][0]
         return float(interest_expense) / float(total_debt)
 
     def calc_equity_cost(self, data, symbol, global_data):
-        treasury_yield = float(global_data['TREASURY_YIELD'])
+        treasury_yield = float(global_data['TREASURY_YIELD']) * 0.01
+        print('treasury yield:', treasury_yield)
         beta = float(data[symbol]['BETA'])
+        print('beta', beta)
+
 
         capm = treasury_yield + beta * (MARKET_RETURN_RATE - treasury_yield)
+        print('capm', capm)
         return capm
 
-    def calc_debt_and_equity_weights(self, data, symbol):
-        total_debt = int(data[symbol]['BALANCE_SHEET']['shortLongTermDebtTotal'][0])
+    def calc_debt_and_equity_weights(self, data, symbol, total_debt):
         market_cap = int(data[symbol]['MARKET_CAPITALIZATION'])
         total = total_debt + market_cap
         return (total_debt / total, market_cap / total)
 
-    def calc_wacc(self, data, symbol, global_data):
+    def calc_wacc(self, data, symbol, global_data, total_debt):
         tax_rate = self.calc_effective_tax_rate(data, symbol)
-        debt_weight, equity_weight = self.calc_debt_and_equity_weights(data, symbol)
+        debt_weight, equity_weight = self.calc_debt_and_equity_weights(data, symbol, total_debt)
+        print('debt/equity weight', debt_weight, ':', equity_weight)
         equity_cost = self.calc_equity_cost(data, symbol, global_data)
-        debt_cost = self.calc_debt_cost_wacc(data, symbol)
-        wacc = debt_weight * debt_cost * (1-tax_rate) + equity_weight * equity_cost
-        self.signals[symbol]['WACC'] = wacc * 0.1
+        debt_cost = self.calc_debt_cost_wacc(data, symbol, total_debt)
+        print('debt/equity cost', debt_cost, ':', equity_cost)
+        wacc = debt_weight * debt_cost * (1 - tax_rate) + equity_weight * equity_cost
+        print('wacc', wacc)
+        self.signals[symbol]['WACC'] = wacc
 
     def calc_terminal_value(self, symbol):
         base_value_last_year_estimate = self.signals[symbol]['PROJECTED_FREE_CASH_FLOWS'][-1]
         print(self.signals[symbol]['PROJECTED_FREE_CASH_FLOWS'])
         print(self.signals[symbol]['WACC'])
-        terminal_value = base_value_last_year_estimate * (1 + PERPETUAL_GROWTH_ESTIMATE) / (self.signals[symbol]['WACC']-PERPETUAL_GROWTH_ESTIMATE)
-        self.signals[symbol]['TERMINAL_VALUE'] = terminal_value
+        terminal_value = base_value_last_year_estimate * (1 + PERPETUAL_GROWTH_ESTIMATE) / (
+                    self.signals[symbol]['WACC'] - PERPETUAL_GROWTH_ESTIMATE)
+        if terminal_value < 0:
+            self.signals[symbol]['TERMINAL_VALUE'] = 0
+        else:
+            self.signals[symbol]['TERMINAL_VALUE'] = terminal_value
 
-    def calc_dfc(self, symbol, period_number):
-        initial_rate = self.signals[symbol]['WACC'] + 1
-        periods = period_number + 1
-        discount_rates = generate_interest_rates(initial_rate, initial_rate, period_number)
-        fcfe_values_to_discount = self.signals[symbol]['PROJECTED_FREE_CASH_FLOWS'] + [self.signals[symbol]['TERMINAL_VALUE']]
-        discounted_npv_for_cash_flows = [dr*fcfe for dr,fcfe in zip(discount_rates, fcfe_values_to_discount)]
+    def calc_dfc(self, symbol, period_number, data):
+        return_multiplier = self.signals[symbol]['WACC'] + 1
+        discount_rates = generate_interest_rates(return_multiplier, period_number)
+        fcfe_values_to_discount = self.signals[symbol]['PROJECTED_FREE_CASH_FLOWS'] + [
+            self.signals[symbol]['TERMINAL_VALUE']]
+        print('fcfe_values_to_discount', fcfe_values_to_discount)
+        discounted_npv_for_cash_flows = [fcfe/dr for dr, fcfe in zip(discount_rates, fcfe_values_to_discount)]
+        print('discounted_npv_for_cash_flows', discounted_npv_for_cash_flows)
         dcf = sum(discounted_npv_for_cash_flows)
-        return dcf
+        print('dcf', dcf)
+        market_cap = int(data[symbol]['MARKET_CAPITALIZATION'])
+        print('market_cap', market_cap)
+        diff = dcf-market_cap
+        print(diff, 'diff')
+        self.signals[symbol]['DCF'] = dcf
+        self.signals[symbol]['DIFF'] = diff
+        self.signals[symbol]['MARKET_CAP'] = market_cap
+
 
 
 signal_calculator = CalculateSignal()
@@ -150,7 +185,7 @@ signal_calculator = CalculateSignal()
 
 @app.route('/')
 async def main_page_finance_data():
-    symbols = ['IBM']
+    symbols = ['IBM', 'NTNX']
     function_types = ['CASH_FLOW', 'INCOME_STATEMENT', 'BALANCE_SHEET']
     tasks = []
     for function_type in function_types:
@@ -164,7 +199,7 @@ async def main_page_finance_data():
         signal_calculator.do_calculations(symbol, financial_data_aggregator.financial_data_aggregate,
                                           financial_data_aggregator.global_data)
     return render_template('data.html', data=financial_data_aggregator.financial_data_aggregate,
-                           calcs=signal_calculator.get_signal())
+                           signals=signal_calculator.get_signal())
 
 
 if __name__ == '__main__':
--- financial_data_aggregator.py
+++ financial_data_aggregator.py
@@ -10,9 +10,12 @@ def create_empty_dict(keys_array):
     return {key: [] for key in keys_array}
 
 
-def add_years_data(financial_data_type, current_year_data):
-    for key in financial_data_type.keys():
-        financial_data_type[key].append(current_year_data[key])
+def add_years_data(sub_category_dict, current_year_data):
+    for key in sub_category_dict.keys():
+        if current_year_data[key] == 'None':
+            sub_category_dict[key].append(0)
+        else:
+            sub_category_dict[key].append(int(current_year_data[key]))
 
 
 def get_sub_category_data(*, data, year_range, keys):
@@ -35,6 +38,9 @@ class FinancialDataTypeSwitch:
 
     def add_error_to_error_state(self, error):
         self.errorState.append(error)
+
+    def get_error_state(self, error):
+        return self.errorState
 
     def set_current_company(self, company):
         self.current_company = company
@@ -73,16 +79,21 @@ class FinancialDataTypeSwitch:
             self.add_error_to_error_state(error)
 
     async def get_price_data(self, symbol):
-        api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={self.api_key}'
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            data = response.json()
-            latest_daily_price_date, latest_daily_price = next(iter(data["Time Series (Daily)"].items()))
-            self.add_to_financial_data_aggregate('LATEST_PRICE', latest_daily_price["4. close"])
-            self.add_to_financial_data_aggregate('LATEST_PRICE_DATE', latest_daily_price_date)
-        else:
-            error = jsonify({'error': f'Failed to fetch overview data for {symbol}'})
+        try:
+            api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={self.api_key}'
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                data = response.json()
+                latest_daily_price_date, latest_daily_price = next(iter(data["Time Series (Daily)"].items()))
+                self.add_to_financial_data_aggregate('LATEST_PRICE', latest_daily_price["4. close"])
+                self.add_to_financial_data_aggregate('LATEST_PRICE_DATE', latest_daily_price_date)
+            else:
+                error = jsonify({'error': f'Failed to fetch overview data for {symbol}'})
+                self.add_error_to_error_state(error)
+        except:
+            error = jsonify({'error': f'Failed to fetch overview data exception {symbol}'})
             self.add_error_to_error_state(error)
+            pass
 
     async def get_treasury_data(self):
         api_url = f'https://www.alphavantage.co/query?function=TREASURY_YIELD&apikey={self.api_key}'
@@ -113,7 +124,7 @@ class FinancialDataTypeSwitch:
                                              get_sub_category_data(data=data, year_range=self.year_range, keys=keys))
 
     def balance_sheet(self, data):
-        keys = ['commonStockSharesOutstanding','shortLongTermDebtTotal']
+        keys = ['commonStockSharesOutstanding','shortTermDebt', 'longTermDebt']
         get_sub_category_data(data=data, year_range=self.year_range, keys=keys)
         self.add_to_financial_data_aggregate('BALANCE_SHEET',
                                              get_sub_category_data(data=data, year_range=self.year_range, keys=keys))
