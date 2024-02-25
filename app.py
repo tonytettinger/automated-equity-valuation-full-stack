0c93cd1eb52ef62e146d38e839a80d2459734775
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-02-25 18:58:08 +0100

    wip: updating routing for static GitHub Pages

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import json
 import sqlite3
 
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 import asyncio
 from functions.financial_data_aggregator import *
@@ -14,6 +15,8 @@ from sql.helpers import database_path, database_access
 load_dotenv()
 
 current_year = datetime.now().year
+
+PROJECT_NAME = 'automated-equity-valuation-full-stack'
 
 app = Flask(__name__, static_url_path='/static')
 limiter = Limiter(
@@ -42,33 +45,39 @@ def handle_global_error(error):
 
 
 @app.route('/')
+def root():
+    return redirect(f'/{PROJECT_NAME}')
+
+
+@app.route(f'/{PROJECT_NAME}')
 def index():
     static = os.listdir('static')
 
     # Generate links for each HTML file
     links = []
     for file in static:
+        if file.startswith('index'):
+            continue
         if file.endswith('.html'):
-            links.append(f'<a href="./{file}">{file}</a>')
-
-    homepage_rendered_html = render_template('home.html', links=links)
+            links.append(f'<a href="/static/{file}">{file}</a>')
+    homepage_rendered_html = render_template('home.html', links=links, project_name=PROJECT_NAME)
     with open('static/index.html', 'w') as f:
-        f.write( homepage_rendered_html)
-    return  homepage_rendered_html
+        f.write(homepage_rendered_html)
+    return homepage_rendered_html
 
 
 async def process_symbols(function_type, symbols):
     tasks = []
     for symbol in symbols:
-            tasks.append(financial_data_aggregator.get_data(function_type, symbol))
-            tasks.append(financial_data_aggregator.get_overview_data(symbol))
-            tasks.append(financial_data_aggregator.get_price_data(symbol))
-            tasks.append(financial_data_aggregator.get_treasury_data())
+        tasks.append(financial_data_aggregator.get_data(function_type, symbol))
+        tasks.append(financial_data_aggregator.get_overview_data(symbol))
+        tasks.append(financial_data_aggregator.get_price_data(symbol))
+        tasks.append(financial_data_aggregator.get_treasury_data())
 
     await asyncio.gather(*tasks)
 
 
-@app.route('/check_stocks', methods=['GET'])
+@app.route(f'/{PROJECT_NAME}/check_stocks', methods=['GET'])
 @limiter.limit("30 per minute")
 async def main_page_finance_data():
     symbols = ['AAPL', 'SSYS', 'HPQ']
@@ -79,22 +88,44 @@ async def main_page_finance_data():
             await process_symbols(function_type, symbols)
         except Exception as e:
             print(f"An error occurred: {e}")
+            # Remove stock that had an error in processing its data while querying
             symbols.remove(e.exception_variable)
-    print('symbols to loop through:', symbols )
 
     if len(symbols) == 0:
         raise Exception('No symbols to loop through')
     for symbol in symbols:
         signal_calculator.do_calculations(symbol, financial_data_aggregator.financial_data_aggregate,
                                           financial_data_aggregator.global_data)
+
+    signals = signal_calculator.get_sorted_dict()
+    financial_data_aggregate = financial_data_aggregator.financial_data_aggregate
+    # Convert signals to JSON format
+    signals_json = json.dumps(signals)
+
+    # Write signals JSON to a file
+    try:
+        with open('static/signals.json', 'w') as f:
+            f.write(signals_json)
+        print("Signals saved to signals.json")
+    except IOError as e:
+        print("Error saving signals to file:", e)
+
+    financial_data_aggregate_json = json.dumps(financial_data_aggregator.financial_data_aggregate)
+    # Write signals JSON to a file
+    try:
+        with open('static/financial_data_aggregate.json', 'w') as f:
+            f.write(financial_data_aggregate_json)
+        print("Data aggregate saved to financial_data_aggregate.json")
+    except IOError as e:
+        print("Error saving signals to file:", e)
+
     rendered_html = render_template('data.html', data=financial_data_aggregator.financial_data_aggregate,
                                     signals=signal_calculator.get_sorted_dict())
-    # Write the rendered HTML to a file
+
+    # Write the rendered HTML to the static folder with a timestamp
     try:
         time_string = datetime.today().strftime('%Y-%m-%d')
         static_path_output_html = 'static/' + time_string + '.html'
-        with open('index.html', 'w') as f:
-            f.write(rendered_html)
         with open(static_path_output_html, 'w') as f:
             f.write(rendered_html)
     except PermissionError as e:
@@ -102,17 +133,44 @@ async def main_page_finance_data():
     except IOError as e:
         print('IOError creating file:', e)
 
-    return rendered_html
+    return 'successfully created the html file for financial data.'
 
 
-@app.route('/settings', methods=['GET'])
+@app.route(f'/{PROJECT_NAME}/show_signals', methods=['GET'])
+async def show_signals():
+    # Read signals data from the file
+    try:
+        with open('static/signals.json', 'r') as f:
+            signals_json = f.read()
+            signals = json.loads(signals_json)
+        print("Signals loaded from signals.json")
+    except FileNotFoundError:
+        print("Signals file not found")
+    except json.JSONDecodeError as e:
+        print("Error decoding signals JSON:", e)
+
+    try:
+        with open('static/financial_data_aggregate.json', 'r') as f:
+            financial_data_aggregate_json = f.read()
+            financial_data_aggregate = json.loads(financial_data_aggregate_json)
+        print("Financial data aggregate loaded from financial_data_aggregate.json")
+    except FileNotFoundError:
+        print("Financial data aggregate file not found")
+    except json.JSONDecodeError as e:
+        print("Error decoding financial data aggregate JSON:", e)
+
+    return render_template('data.html', data=financial_data_aggregate,
+                           signals=signals)
+
+
+@app.route(f'/{PROJECT_NAME}/settings', methods=['GET'])
 def settings():
     columns, values = get_variables_from_db()
     column_value_zip = zip(columns, values)
     return render_template('settings.html', columns=columns, values=values, column_value_zip=column_value_zip)
 
 
-@app.route('/update_value', methods=['POST'])
+@app.route(f'/{PROJECT_NAME}/update_value', methods=['POST'])
 def update_value():
     database_access.update_selected_value()
     return redirect('/settings')
