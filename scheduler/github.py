27c9125011ae41e9fe5c24a798faa197134e8cc0
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-03-10 08:20:59 +0100

    2024-03-10 commit message

--- scheduler/github.py
+++ scheduler/github.py
@@ -12,9 +12,10 @@ def add_all_and_commit():
 
     print(git.status())
     print(current_directory)
-    print(git.add('../templates/home.html'))
+    print(git.add('../static/'))
     print(git.commit(m=f'{time_string} commit message'))
     print(git.push())
     print(git.status())
 
 add_all_and_commit()
+print('Successfully committed files to GitHub')
