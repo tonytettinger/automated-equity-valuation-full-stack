5e9014c5a34949a1867273c36a374be278076a56
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-03-12 19:32:37 +0100

    update: scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -2,16 +2,12 @@ import requests
 import schedule
 import time
 
-from notifications import notify
-
 TIME_ZONE = 'America/New_York'
 AFTER_MARKET_CLOSING_TIME = '16:01'
 
-def job():
-    print("I'm working...")
 
 def request_local_endpoint():
-    endpoint = 'http://127.0.0.1:5000//check_stocks?scheduler=true'
+    endpoint = 'http://127.0.0.1:5000/check_stocks?scheduler=true'
     try:
         response = requests.get(endpoint)
         if response.status_code == 200:
@@ -21,8 +17,8 @@ def request_local_endpoint():
     except Exception as e:
         print(f"Error occurred while requesting local endpoint: {e}")
 
-schedule.every().minute.do(request_local_endpoint)
 
+schedule.every().minute.do(request_local_endpoint)
 
 while True:
     schedule.run_pending()
