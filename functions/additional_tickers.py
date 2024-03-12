de1f11d7102c750254f51b6806b40749c3890c3c
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-03-12 19:23:35 +0100

    feat: adding biggest loser query possibility to additional_tickers.py

--- functions/additional_tickers.py
+++ functions/additional_tickers.py
@@ -10,3 +10,16 @@ async def get_tech_stock_market_movers():
     soup = BeautifulSoup(page.content, "html.parser")
     tickers_container = soup.findAll('a', attrs={'data-test': 'symbol-link'})
     return [ticker.get_text() for ticker in tickers_container]
+
+async def get_biggest_losers():
+    try:
+        URL = "https://finance.yahoo.com/losers/"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+        page = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        tickers_container = soup.findAll('a', attrs={'data-test': 'quoteLink'})
+
+        return [ticker.get_text() for ticker in tickers_container]
+    except:
+        return []
