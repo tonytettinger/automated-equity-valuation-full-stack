a546aca1fb712792cf97db98616c09f67aacb7ac
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-03-15 15:11:22 +0100

    fix: updating static link generator

--- functions/get_links_from_static.py
+++ functions/get_links_from_static.py
@@ -2,9 +2,11 @@ import os
 
 
 def get_links_from_static():
-    static = os.listdir('../static')
     links = []
-    for file in static:
+
+    static_dir = os.path.abspath(os.path.join(__file__, '../../static'))
+
+    for file in os.listdir(static_dir):
         if file.startswith('index'):
             continue
         if file.endswith('.html'):
